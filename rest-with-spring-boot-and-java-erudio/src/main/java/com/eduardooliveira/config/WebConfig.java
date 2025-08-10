4e7f2444903e640bcd70bf3d94d643d96cb74f68
Eduardo Oliveira <eduoliveira5002@gmail.com>
2025-08-10 12:19:37 -0300

    Add XML support for Person API

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/eduardooliveira/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.eduardooliveira.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+                .favorParameter(true) // Usa parâmetro ?mediaType=json
+                .parameterName("mediaType")
+                .ignoreAcceptHeader(false) // Respeita o cabeçalho Accept
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+
+}
--- rest-with-spring-boot-and-java-erudio/src/main/java/com/eduardooliveira/dto/PersonDTO.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/eduardooliveira/dto/PersonDTO.java
@@ -1,18 +1,31 @@
 package com.eduardooliveira.dto;
 
+import com.eduardooliveira.serializer.GenderSerializer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 public class PersonDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
 
     private Long id;
+
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
+
+    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
 
     public PersonDTO() {
